0afbbc575a1d0eae7d96d0665344defcd12c8f12
Alan Jeferson <alan.jeferson11@gmail.com>
2015-08-22 11:26:43 -0300

    Scroll view now paging and zooming with pich (far from good enaough)

--- AJImageViewController/AJImageViewController.swift
+++ AJImageViewController/AJImageViewController.swift
@@ -9,15 +9,20 @@
 import UIKit
 
 class AJImageViewController: UIViewController, UIScrollViewDelegate {
-
+    
     @IBOutlet weak var scrollView: UIScrollView!
     var imageView: UIImageView!
+    
+    var images = [UIImage]()
+    var imageViews = [UIImageView?]()
+    var innerScrollFrame: CGRect!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.blackColor()
-        self.setupImageView()
-        self.setupScrollView()
+//        self.setupImageView()
+//        self.setupScrollView()
+        self.setupPagging()
     }
     
     func setupImageView() -> Void {
@@ -31,7 +36,10 @@ class AJImageViewController: UIViewController, UIScrollViewDelegate {
     
     func setupScrollView() -> Void {
         self.scrollView.delegate = self
-        self.scrollView.bounces = false
+        
+        let v = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        v.backgroundColor = UIColor.greenColor()
+        self.scrollView.addSubview(v)
         
         //Setup double tap gesture
         self.scrollView.contentSize = self.imageView.frame.size
@@ -47,15 +55,16 @@ class AJImageViewController: UIViewController, UIScrollViewDelegate {
         let minScale = min(scaleWidth, scaleHeight)
         self.scrollView.minimumZoomScale = minScale
         
-        self.scrollView.maximumZoomScale = 1.0
+        self.scrollView.maximumZoomScale = 3.0
         self.scrollView.zoomScale = minScale
         
-        self.centerScrollViewContents()
+//        self.centerScrollViewContents()
+        
     }
     
-    func centerScrollViewContents() -> Void {
-        let boundsSize = self.scrollView.bounds.size
-        var contentFrame = self.imageView.frame
+    func center(#imageView: UIImageView, ofScrollView scrollView: UIScrollView) -> Void {
+        let boundsSize = scrollView.bounds.size
+        var contentFrame = imageView.frame
         
         if contentFrame.size.width < boundsSize.width {
             contentFrame.origin.x = (boundsSize.width - contentFrame.size.width) / 2.0
@@ -89,14 +98,155 @@ class AJImageViewController: UIViewController, UIScrollViewDelegate {
         
     }
     
+    func zoom(#scrollView: UIScrollView, toScale scale: CGFloat, animated: Bool) -> Void {
+        let scrollViewSize = scrollView.bounds.size
+        let w = scrollViewSize.width / scale
+        let h = scrollViewSize.height / scale
+//        let x = self.view.center.x - (w/2.0)
+//        let y = self.view.center.y - (h/2.0)
+        let rectToZoom = CGRect(x: 0.0, y: 0.0, width: w, height: h)
+        scrollView.zoomToRect(rectToZoom, animated: animated)
+    }
+    
+    func setupPagging() -> Void {
+        self.scrollView.delegate = self
+        
+        self.images.append(UIImage(named: "jake")!)
+        self.images.append(UIImage(named: "fin")!)
+        self.images.append(UIImage(named: "iceKing")!)
+        
+        self.innerScrollFrame = self.scrollView.bounds
+        
+        for i in 0..<self.images.count {
+            //Init inside image
+            let imageForZooming = UIImageView(image: self.images[i])
+            imageForZooming.tag = 11
+            
+            //Init inside scroll that holds the image
+            let insideScroll = UIScrollView(frame: innerScrollFrame)
+            insideScroll.showsHorizontalScrollIndicator =  false
+            insideScroll.showsVerticalScrollIndicator = false
+            insideScroll.contentSize = imageForZooming.bounds.size
+            insideScroll.delegate = self
+            
+            //Setting up the max an min zoomscales for not allowing to zoom out more than the image size
+            let scrollViewFrame = insideScroll.frame
+            let scaleWidth = scrollViewFrame.size.width / insideScroll.contentSize.width
+            let scaleHeight = scrollViewFrame.size.height / insideScroll.contentSize.height
+            let minScale = min(scaleWidth, scaleHeight)
+            insideScroll.minimumZoomScale = minScale
+            insideScroll.maximumZoomScale = 3.0
+            insideScroll.zoomScale = minScale
+            
+            //Adding subviews
+            insideScroll.addSubview(imageForZooming)
+            self.scrollView.addSubview(insideScroll)
+            
+            //Zooming to aspect fit the screen
+            self.zoom(scrollView: insideScroll, toScale: minScale, animated: false)
+            
+            //Centering the image
+            self.center(imageView: imageForZooming, ofScrollView: insideScroll)
+            
+            //Incrementing the inner rect origin to hold the next scroll in the right position
+            innerScrollFrame.origin.x += innerScrollFrame.size.width
+            
+        }
+        
+        self.scrollView.contentSize = CGSize(width: innerScrollFrame.origin.x, height: self.scrollView.bounds.size.height)
+        
+//        self.loadVisiblePages()
+    }
+    
+    func load(#page: Int) -> Void {
+        if page>=0 && page<self.images.count {
+            if self.imageViews[page] == nil {
+//                //Page already loaded
+//                var frame = self.scrollView.bounds
+//                frame.origin.x = frame.size.width * CGFloat(page)
+//                frame.origin.y = 0.0
+                var origin = CGPoint(x: self.images[page].size.width * CGFloat(page), y: 0.0)
+//
+//                let newPageView = UIImageView(image: self.images[page])
+////                newPageView.contentMode = UIViewContentMode.ScaleAspectFit
+//                newPageView.frame.origin.x = frame.origin.x
+//                newPageView.frame.origin.y = 0.0
+//                self.scrollView.contentSize = newPageView.frame.size
+//                self.scrollView.addSubview(newPageView)
+//                
+//                self.imageViews[page] = newPageView
+//                
+//                self.scrollView.delegate = self
+//                self.scrollView.zoomScale = 2.0
+                
+                self.imageView = UIImageView(image: self.images[page])
+                self.imageView.frame = CGRect(origin: origin, size: self.images[page].size)
+                self.scrollView.addSubview(self.imageView)
+                
+                self.scrollView.delegate = self
+                
+                //Setup double tap gesture
+//                self.scrollView.contentSize = self.imageView.frame.size
+                var doubleTapGesture = UITapGestureRecognizer(target: self, action: Selector("doubleTapScrollView:"))
+                doubleTapGesture.numberOfTapsRequired = 2
+                doubleTapGesture.numberOfTouchesRequired = 1
+                self.scrollView.addGestureRecognizer(doubleTapGesture)
+                
+                //Setup scroll view first scale
+                let scrollViewFrame = self.scrollView.frame
+                let scaleWidth = scrollViewFrame.size.width / self.scrollView.contentSize.width
+                let scaleHeight = scrollViewFrame.size.height / self.scrollView.contentSize.height
+                let minScale = min(scaleWidth, scaleHeight)
+                self.scrollView.minimumZoomScale = minScale
+                
+                self.scrollView.maximumZoomScale = 3.0
+                self.scrollView.zoomScale = minScale
+                
+//                self.centerScrollViewContents()
+            }
+        }
+    }
+    
+    func purge(#page: Int) -> Void {
+        if page>=0 && page<self.images.count {
+            if let pageView = self.imageViews[page] {
+                pageView.removeFromSuperview()
+                self.imageViews[page] = nil
+            }
+        }
+    }
+    
+    func loadVisiblePages() -> Void {
+        let pageWidth = self.scrollView.frame.size.width
+        let page = Int(floor((scrollView.contentOffset.x * 2.0 + pageWidth) / (pageWidth * 2.0)))
+        let firstPage = page - 1
+        let lastPage = page + 1
+        
+        for var index = 0; index<firstPage; ++index {
+            self.purge(page: index)
+        }
+        
+        for index in firstPage...lastPage {
+            self.load(page: index)
+        }
+        
+        for var index = lastPage+1; index<self.images.count; ++index {
+            self.purge(page: index)
+        }
+    }
     
     //MARK:- ScrollView delegate
     func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
-        return self.imageView
+//        return self.imageView
+        return scrollView.viewWithTag(11)
     }
     
     func scrollViewDidZoom(scrollView: UIScrollView) {
-        self.centerScrollViewContents()
+//        self.centerScrollViewContents()
+    }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+//        self.loadVisiblePages()
     }
 }
 
